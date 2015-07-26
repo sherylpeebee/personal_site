56b3b985321f927f9801937781412c8f26c9c5da
Sheryl Boughton <sherpbo@gmail.com>
2015-07-26 12:57:53 -0700

    include typed interaction

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 $(function () {
-    $('.curtains').curtain({
-        scrollSpeed: 400,
-        controls: '.menu'
-    });
+  $('.curtains').curtain({
+      scrollSpeed: 400,
+      controls: '.menu',
+      nextSlide: function() { console.log('next slide'); },
+      prevSlide: function() { console.log('previous slide'); }
+  });
+
+  $('.like').fadeIn(2000);
+
+  setTimeout(function(){
+    $('.list')
+    .typetype('make things pretty!')
+    .delay(1000)
+    .backspace(20)
+    .typetype('make things smart.')
+    .delay(1000)
+    .backspace(18)
+    .typetype('make things work.')
+    .delay(1000)
+    .backspace(30)
+    .typetype('make things! ^_^ ');
+    // .fadeOut(); // regular jQuery effects queue up nicely
+  }, 2000);
+
+
 });
