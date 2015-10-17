04013a61fa371f368a78e9ce5526d35226dc3ec4
Sheryl Boughton <sherpbo@gmail.com>
2015-10-16 21:36:06 -0700

    begin to style a side nav

--- script.js
+++ script.js
@@ -6,23 +6,25 @@ $(function () {
       prevSlide: function() { console.log('previous slide'); }
   });
 
-    $('.popup-gallery').magnificPopup({
-      delegate: 'a',
-      type: 'image',
-      // tLoading: 'Loading image #%curr%...',
-      mainClass: 'mfp-img-mobile',
-      gallery: {
-        enabled: true,
-        navigateByImgClick: true,
-        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-      }
-      // image: {
-      //   tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      //   titleSrc: function(item) {
-      //     return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-      //   }
-      // }
-    });
+  $pages = $('.curtain-page');
+
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    // tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    }
+    // image: {
+    //   tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    //   titleSrc: function(item) {
+    //     return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+    //   }
+    // }
+  });
 
 list = $('.selfies');
 
