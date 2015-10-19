6f9561b2ac074ee643be9fffe775cbcaa760b060
Sheryl Boughton <sherpbo@gmail.com>
2015-10-19 01:06:50 -0700

    fix summaries to show dynamically with multiple projects

--- script.js
+++ script.js
@@ -55,48 +55,49 @@ $(function () {
     .typetype('make.allTheThings( )');
   }, 7000);
 
-$('li.nav_to_page.tab-li ').on('click', function(){
-  if($('.side-nav').hasClass('inactive')){
-    $('.side-nav').removeClass('inactive').addClass('active');
-  }
-  else if($('.side-nav').hasClass('active')){
-    $('.side-nav').removeClass('active').addClass('inactive');
-  }
-});
-
-var projects =
-[
-  {
-    title: 'the history of us',
-    tech_icon: '/assets/tech_icons/mean_icon.jpg',
-    views: [
-      {img: '/assets/projects/history_splash.png', summary: 'some stuff happened some stuff happened some stuff happened some stuff happened some stuff happened some stuff happened'},
-      {img: '/assets/projects/history_blank.png', summary: 'fbjsdb hfsjv cd fsdji a hnkg '},
-      {img: '/assets/projects/history_storyForm.png', summary: '0000000 fdsxfd dwhs fsdn fharuhgdj'}
-    ],
-    link: 'https://the-history-of-us.herokuapp.com/#/'
-  }
-];
-
-
-  var $currentProjectTitle = $('#project-title').text().toLowerCase().trim();
-  console.log($currentProjectTitle);
-  var currentProject;
-  projects.forEach(function(project){
-    if(project.title === $currentProjectTitle){
-      currentProject = project;
+  $('li.nav_to_page.tab-li ').on('click', function(){
+    if($('.side-nav').hasClass('inactive')){
+      $('.side-nav').removeClass('inactive').addClass('active');
+    }
+    else if($('.side-nav').hasClass('active')){
+      $('.side-nav').removeClass('active').addClass('inactive');
     }
   });
-  console.log(currentProject);
+
+  var projects =
+  [
+    {
+      title: 'the history of us',
+      tech_icon: '/assets/tech_icons/mean_icon.jpg',
+      views: [
+        {img: '/assets/projects/history_splash.png', summary: 'some stuff happened some stuff happened some stuff happened some stuff happened some stuff happened some stuff happened'},
+        {img: '/assets/projects/history_blank.png', summary: 'fbjsdb hfsjv cd fsdji a hnkg '},
+        {img: '/assets/projects/history_storyForm.png', summary: '0000000 fdsxfd dwhs fsdn fharuhgdj'}
+      ],
+      link: 'https://the-history-of-us.herokuapp.com/#/'
+    }
+  ];
 
   $('img.projects:not(.project_hero)').on('click', function(e){
     e.stopPropagation();
+
+    var $currentProjectTitle = $(this).parent().parent()
+    .children('#project-title').text().toLowerCase().trim();
+    console.log($currentProjectTitle);
+    var currentProject;
+    projects.forEach(function(project){
+      if(project.title === $currentProjectTitle){
+        currentProject = project;
+      }
+    });
+    console.log(currentProject);
+
     var $currentHero = $(this).parent().parent().children('.project_hero');
     var newSrc = $(this).attr('src');
     var oldSrc = $($currentHero).attr('src');
     $($currentHero).attr('src', newSrc);
     $(this).attr('src', oldSrc);
-    var $currentView = $('img.project_hero').attr('src');
+    var $currentView = $(this).parent().parent().children('img.project_hero').attr('src');
     console.log($currentView);
     var summary;
     currentProject.views.forEach(function(view){
@@ -104,7 +105,7 @@ var projects =
         summary = view.summary;
       }
     });
-    $('.summary').text(summary);
+    $(this).parent().parent().children('.summary').text(summary);
   });
 
   var project_pages = $('.project_page');
@@ -112,24 +113,23 @@ var projects =
   var counter = totalPages-1;
   var project_page_width = $('.project_page').width();
 
-$('.project_page').click(function(e){
-  console.log(counter);
-  e.stopPropagation();
-  if(counter === 0){
-    $('.project_page').css('transform', '');
-    counter = totalPages;
-  }
-  else{
-    $(project_pages[counter]).css('transform', 'translate(-'+ project_page_width + 'px)');
-  }
-  counter --;
+  $('.project_page').click(function(e){
+    console.log(counter);
+    e.stopPropagation();
+    if(counter === 0){
+      $('.project_page').css('transform', '');
+      counter = totalPages;
+    }
+    else{
+      $(project_pages[counter]).css('transform', 'translate(-'+ project_page_width + 'px)');
+    }
+    counter --;
+  });
 
-});
-
-$('.btn').click(function(e){
-  e.stopPropagation();
-  alert('works');
-});
+  $('.btn').click(function(e){
+    e.stopPropagation();
+    alert('works');
+  });
 
 
 });
